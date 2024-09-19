9b6d5baab05de8482d357255ebb85e18d9b6d131
anton <friendlyantz@pm.me>
2024-09-19 18:11:40 +1000

    Add HTMX to Sinatra and basic form.
    disable js spec in CI due to time presuure

--- lib/app.rb
+++ lib/app.rb
@@ -9,6 +9,7 @@ get "/", provides: "html" do
     <!DOCTYPE html>
       <html>
         <head>
+        <script src="htmx/htmx.min.js"></script>
           <title>Term Deposit Calculator</title>
           <meta charset="UTF-8">
         </head>
@@ -17,7 +18,80 @@ get "/", provides: "html" do
 
      <h2>Term Deposit Calculator</h2>
 
+     <button hx-get="/cash_form" hx-target="#cash_form" hx-swap="innerHTML">
+       Cash
+     </button>
+
+     <button hx-get="/term_form"  hx-target="#cash_form" hx-swap="innerHTML">
+       TermDeposit
+     </button>
+
+    <form  hx-post="/calculate" hx-target="#table" hx-swap="innerHTML>
+
+       <label for="principal">Starting with (Principal):</label>
+       <input type="text" id="principal" name="principal"
+             hx-post="/calculate"
+             hx-trigger="change"
+             placeholder="10000.00"><br>
+
+       <label for="interest_rate">Interest Rate p.a.:</label>
+       <input type="text" id="interest_rate" name="interest_rate"
+             hx-post="/calculate"
+             hx-trigger="change"
+             placeholder="0.12"><br>
+
+       <label for="term">Term in Years:</label>
+       <input type="text" id="term" name="term"
+             hx-post="/calculate"
+             hx-trigger="change"
+             placeholder="1.5"><br>
+
+       <div id="cash_form">
+         <p style="color: red;"> Please select Cash or TermDeposit </p>
+       </div>
+
+       <input type="submit" value="Re-Calculate">
+    </form>
+
+        <div id="table">
+        <p style="color: red;"> Please fill the form above </p>
+        </div>
+
       </body>
     </html>
   HTML
+end
+
+post "/calculate" do
+  "Calculating..."
+end
+
+def compounting_freq_picker(period_name)
+  <<~HTML
+    <input type="radio" id="#{period_name}" name="compounding" value="#{period_name}"
+          hx-post="/calculate" hx-trigger="change" >
+    <label for="#{period_name}">#{period_name}</label>
+  HTML
+end
+
+get "/cash_form" do
+  extra_deposit_form = <<~HTML
+    <label for="top_up">Extra deposit:</label>
+    <input type="text" id="top_up" name="top_up" hx-post="/calculate" hx-trigger="change" placeholder="0.00" >
+    <div> Extra deposit every: </div>
+  HTML
+
+  extra_deposit_form +
+    compounting_freq_picker("Weekly") +
+    compounting_freq_picker("Fortnightly") +
+    compounting_freq_picker("Monthly") +
+    compounting_freq_picker("Annually")
+end
+
+get "/term_form" do
+  "Interest paid:<br>" +
+    compounting_freq_picker("Monthly") +
+    compounting_freq_picker("Quarterly") +
+    compounting_freq_picker("Annually") +
+    compounting_freq_picker("Maturity")
 end
--- spec/e2e/cash_and_term_deposit_spec.rb
+++ spec/e2e/cash_and_term_deposit_spec.rb
@@ -9,9 +9,29 @@ RSpec.describe "Cash and Term DepositCalculator", :js, type: :feature do
     end
 
     Then "I should see errors prompting to fill the form" do
-      pending
       expect(page).to have_content("Please select Cash or TermDeposit")
       expect(page).to have_content("Please fill the form")
+    end
+
+    When "I select Term Deposit" do
+      click_button "TermDeposit"
+    end
+
+    Then "relevant errors should dissapear" do
+      expect(page).to have_content("Please fill the form")
+      expect(page).not_to have_content("Please select Cash or TermDeposit")
+    end
+
+    When "I fill the form with INVALID params" do
+      fill_in "principal", with: "invalid input"
+      fill_in "interest_rate", with: 99_999_999_999
+      fill_in "term", with: -1
+      choose "Monthly"
+    end
+
+    Then "I should see errors prompting to fill the form" do
+      pending
+      expect(page).to have_content("Some Validation error")
     end
   end
 end
